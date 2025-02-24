81d472ad62bac08648a43a9d781429a1224e6e53
Cristina Zoccola <cristina.zoccola@studio.unibo.it>
2025-02-24 11:19:46 +0100

    Setup tests

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,10 +1,19 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    private SimpleSmartDoorLock smartDoorLock;
+
+    @BeforeEach
+    void beforeEach() {
+        int initialPin = 1234;
+        smartDoorLock = new SimpleSmartDoorLock(initialPin);
+    }
+
     @Test
     public void todo() {
         assertTrue(true);
